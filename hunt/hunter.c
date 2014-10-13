f8fcbfb42b9f165340e083a3788088d777909153
TaraTjandra <tara_tjandra@hotmail.com>
2014-10-14 03:38:39 +1100

    A dumb random hunt
    We might need an ID to abbrev function to get from LocationID’s to
    bestPlay strings. Am I just tired and missing something or should we
    edit Places?

--- hunt/hunter.c
+++ hunt/hunter.c
@@ -6,9 +6,105 @@
 #include "Game.h"
 #include "HunterView.h"
 
-void decideHunterMove(HunterView gameState)
-{
-    // TODO ...
-    // Replace the line below by something better
-    registerBestPlay("GE","I'm on holiday in Geneva");
+void decideHunterMove(HunterView gameState) {
+    PlayerID id = whoAmI(gameState);
+    char *bestPlay = "";
+    
+    if (id == PLAYER_DR_SEWARD) {
+        if (whereIs(gameState,id) == UNKNOWN_LOCATION) {
+            bestPlay = "CD";
+        } else if (whereIs(gameState,id) == ST_JOSEPH_AND_ST_MARYS) {
+            bestPlay = "SZ";
+        } else if (whereIs(gameState,id) == CASTLE_DRACULA) {
+            bestPlay = "KL";
+        } else if (whereIs(gameState,id) == KLAUSENBURG) {
+            bestPlay = "SZ";
+        } else if (whereIs(gameState,id) == SZEGED) {
+            bestPlay = "BC";
+        } else if (whereIs(gameState,id) == BUCHAREST) {
+            bestPlay = "GA";
+        } else if (whereIs(gameState,id) == GALATZ) {
+            bestPlay = "CD";
+        }
+    } else if (id == PLAYER_LORD_GOLDAMING) {
+        if (whereIs(gameState,id) == UNKNOWN_LOCATION) {
+            bestPlay = "SR";
+        } else {
+            LocationID current = whereIs(gameState, id);
+            int *numLocations;
+            LocationID *connections = whereCanTheyGo(gameState, numLocations, id, TRUE, TRUE, TRUE);
+            int counter = 0;
+            LocationID next = NUM_MAP_LOCATIONS;
+            LocationID lowest = current;
+            while (counter < numLocations[0]) {
+                LocationID city = connections[counter];
+                if (city > current && city < next) {
+                    next = city;
+                } else if (city < lowest) {
+                    lowest = city;
+                }
+                counter++;
+            }
+            if (next != NUM_MAP_LOCATIONS) {
+                bestPlay = idToAbbrev(next);
+                // IMPORTANT: THERE IS NO idToAbbrev() FUNCTION, SHOULD WE ALTER PLACES.C TO MAKE ONE???
+            } else {
+                bestPlay = idToAbbrev(lowest);
+            }
+        }
+    } else if (id == PLAYER_VAN_HELSING) {
+        if (whereIs(gameState,id) == UNKNOWN_LOCATION) {
+            bestPlay = "HA";
+        } else {
+            LocationID current = whereIs(gameState, id);
+            int *numLocations;
+            LocationID *connections = whereCanTheyGo(gameState, numLocations, id, TRUE, TRUE, TRUE);
+            int counter = 0;
+            LocationID next = UNKNOWN_LOCATION;
+            LocationID highest = current;
+            while (counter < numLocations[0]) {
+                LocationID city = connections[counter];
+                if (city < current && city > next) {
+                    next = city;
+                } else if (city > highest) {
+                    highest = city;
+                }
+                counter++;
+            }
+            if (next != UNKNOWN_LOCATION) {
+                bestPlay = idToAbbrev(next);
+                // IMPORTANT: THERE IS NO idToAbbrev() FUNCTION, SHOULD WE ALTER PLACES.C TO MAKE ONE???
+            } else {
+                bestPlay = idToAbbrev(highest);
+            }
+        }
+    } else if (id == PLAYER_MINA_HARKER) {
+        if (whereIs(gameState,id) == UNKNOWN_LOCATION) {
+            bestPlay = "VE";
+        } else {
+            LocationID current = whereIs(gameState, id);
+            int *numLocations;
+            LocationID *connections = whereCanTheyGo(gameState, numLocations, id, TRUE, TRUE, TRUE);
+            int counter = 0;
+            LocationID next = UNKNOWN_LOCATION;
+            LocationID highest = current;
+            while (counter < numLocations[0]) {
+                LocationID city = connections[counter];
+                if (city < current && city > next) {
+                    next = city;
+                } else if (city > highest) {
+                    highest = city;
+                }
+                counter++;
+            }
+            if (next != UNKNOWN_LOCATION) {
+                bestPlay = idToAbbrev(next);
+                // IMPORTANT: THERE IS NO idToAbbrev() FUNCTION, SHOULD WE ALTER PLACES.C TO MAKE ONE???
+            } else {
+                bestPlay = idToAbbrev(highest);
+            }
+        }
+    }
+    
+    registerBestPlay(bestPlay,"A semi-randomly moving AI");
 }
