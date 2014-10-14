a9137f41576b6d1a2fe53448a6d48fb4482e6c61
gabyfebriana <gabriela@a.local>
2014-10-14 18:44:50 +1100

    idToAbrev in .h

--- hunt/Places.h
+++ hunt/Places.h
@@ -129,6 +129,9 @@ int nameToID(char *name);
 // given a Place abbreviation, return its ID number
 int abbrevToID(char *abbrev);
 
+// given a Place ID return its abbreviation (2 char)
+char *idToAbbrev(LocationID p);
+
 #define isLand(place)  (typeOf(place) == LAND)
 #define isSea(place)  (typeOf(place) == SEA)
 
